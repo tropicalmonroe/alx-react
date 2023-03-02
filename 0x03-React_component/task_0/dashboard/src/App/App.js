d8d2c0404902a5e1d3fada4de184a9f1beca54ed
tropicalmonroe <shitsamajohn821@gmail.com>
2023-03-01 23:46:13 -0800

    task complete fix

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, {Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Notification from '../Notifications/Notifications';
 import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList'
 import { getLatestNotification } from '../utils/utils';
-import PropTypes from 'prop-types';
 
 const listCourses = [
-    {id: 1, name: 'ES6', credit: 60},
-    {id: 2, name: 'Webpack', credit: 20},
-    {id: 3, name: 'React', credit: 40}
+  {id: 1, name: "ES6", credit: 60},
+  {id: 2, name: "Webpack", credit: 20},
+  {id: 3, name: "React", credit: 40}
 ]
-
 const listNotifications = [
-    {id: 1, value: "New course available", type: "default"},
-    {id: 2, value: "New resume available", type: "urgent"},
-    {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
+  {id: 1, type:'default', value:"New course available"},
+  {id: 2, type:'urgent', value:"New resume available"},
+  {id: 3, type:'urgent', html:{__html: getLatestNotification() }}
 ]
 
 class App extends Component {
