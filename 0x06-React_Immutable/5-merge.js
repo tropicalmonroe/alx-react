6aef153dd415f48cacef06b42464add6cf229c20
tropicalmonroe <shitsamajohn821@gmail.com>
2023-03-15 05:29:54 -0700

    task done fix

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,15 @@
 import { Map, List } from 'immutable';
 
 export function concatElements(page1, page2) {
-  const list_1 = List(page1);
-  const list_2 = List(page2);
+  const list1 = List(page1);
+  const list2 = List(page2);
 
-  return list_1.concat(list_2);
+  return list1.concat(list2);
 }
 
 export function mergeElements(page1, page2) {
-  const map_1 = Map(page1);
-  const map_2 = Map(page2);
+  const map1 = Map(page1);
+  const map2 = Map(page2);
 
-  return map_1.merge(map_2);
+  return map1.merge(map2);
 }
