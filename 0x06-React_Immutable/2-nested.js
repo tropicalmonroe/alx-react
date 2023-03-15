4bcce8848f544d681de9d535c7473c3a4ad2949b
tropicalmonroe <shitsamajohn821@gmail.com>
2023-03-15 05:13:08 -0700

    Task done fix

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -2,6 +2,6 @@ import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
-  
+
   return mappedObj.getIn(array, undefined);
 }
