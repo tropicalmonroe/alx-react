38dd7ad9fda5640c2c7d066b7033657d0d1559b9
tropicalmonroe <shitsamajohn821@gmail.com>
2023-04-03 14:06:16 -0700

    redux task 2

---
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import App, { listNotificationsInitialState, mapStateToProps } from "./App";
+import { shallow, mount } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+import AppContext, { user, logOut } from "./AppContext";
+import { fromJS } from "immutable";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import uiReducer, {initialState} from "../reducers/uiReducer";
+
+const store = createStore(uiReducer, initialState);
+
+describe("<App />", () +> {
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = fromJS({
+      isUserLoggedIn: true,
+    });
+
+    const result = mapStateToProps(state);
+
+    except(result).toEqual({ isLoggedIn: true });
+  });
+
+  it("mapStateToProps returns the right object from display Drawer", () => {
+    let state = fromJS ({
+      isNotificationDrawerVisible: true,
+    });
+
+    const result = mapStateToProps(state);
+
+    except(result).toEqual({ isLoggedIn: true });
+
+  });
+});
