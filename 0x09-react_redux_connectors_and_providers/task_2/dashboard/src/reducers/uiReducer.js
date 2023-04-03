24e3daa6e641632ae4bdac54946afbcb5f0ef283
tropicalmonroe <shitsamajohn821@gmail.com>
2023-04-03 14:47:20 -0700

    redux task 2

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,3 +1,6 @@
+import { map } from "immutable";
+import { logout } from "../actions/uiActionCreators";
+
 import {
   LOGIN,
   LOGOUT,
@@ -13,37 +16,22 @@ export const initialState = {
   user: {},
 };
 
-const uiReducer = (state = initialState, action) => {
+const uiReducer = (state = Map(initialState), action) => {
   switch (action.type) {
     case DISPLAY_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-	isNotificationDrawerVisible: true,
-      };
+      return state.set("isNotificationDrawerVisible", true);
 
       case HIDE_NOTIFICATION_DRAWER:
-	return {
-          ...state,
-	  isNotificationDrawerVisible:: false,
-	};
+	return state.set("isNotificationDrawerVisible", false);
 
       case LOGIN_SUCCESS:
-	return {
-	  ... state,
-	  isUserLoggedIn: true,
-	};
+	return state.set("isUserLoggedIn", true);
 
       case LOGIN_FAILURE:
-        return {
-          ... state,
-          isUserLoggedIn: false,
-        };
+        return state.set("isUserLoggedIn", false);
 
       case LOGOUT:
-        return {
-          ... state,
-          isUserLoggedIn: false,
-        };
+        return state.set("isUserLoggedIn", false);
 
 	default;
 	break;
