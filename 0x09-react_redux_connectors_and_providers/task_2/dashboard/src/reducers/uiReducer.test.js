66bfd19e6f6751b6d92b97928f2a9ea7415370c5
tropicalmonroe <shitsamajohn821@gmail.com>
2023-04-04 02:47:30 -0700

    redux task 2

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.test.js
@@ -1,17 +1,34 @@
 import uiReducer, { initialState } from './uiReducer';
-import { DISPLAY_NOTIFICATION_DRAWER  } from '../actions/uiActionTypes';
+import { LOGIN, DISPLAY_NOTIFICATION_DRAWER  } from '../actions/uiActionTypes';
 
-describe('reducer', function () {
-  it('initial state', function () {
+const USER = { email: "shiti@gmail.com", password: "1234567" };
+
+describe("uiReducer tests", function () {
+  it("function equals the initial state when no action is passed", function () {
     const state = uiReducer(undefined, {});
-    expect(state).toEqual(initialState);
-  });
 
-  it('DISPLAY_NOTIFICATION_DRAWER', function () {
+     expect(state.toJS()).toEqual(initialState);
+  });
+  it(" function equals the initial state when the action SELECT_COURSE is passed", function () {
+    const state = uiReducer(undefined, { type: "SELECT_COURSE" });
+
+    expect(state.toJS()).toEqual(initialState);
+  });
+  it("function, when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property", function () {
     const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
-    expect(state).toEqual({
+
+    expect(state.toJS()).toEqual({
       ...initialState,
       isNotificationDrawerVisible: true,
+    });
+  });
+
+  it("verifies the state returned by the uiReducer function, when the action LOGIN is passed, changes correctly the user property", function () {
+    const state = uiReducer(undefined, { type: LOGIN, user: USER });
+
+    expect(state.toJS()).toEqual({
+      ...initialState,
+      user: USER,
     });
   });
 });
